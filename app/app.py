768041027b70df4858f37d48eaed6bbb6fa8ab83
UrianV <urian1213viera@gmail.com>
2023-05-15 20:22:00 -0500

    comentando el archivo app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 
 # Declarando nombre de la aplicación e inicializando, crear la aplicación Flask
 app = Flask(__name__)
-application = app
-app.secret_key = '97110c78ae51a45af397be6534caef90ebb9b1dcb3380af008f90b23a5d1616bf19bc29098105da20fe'
 
+
+ # Definir y asignar un valor a current_value
 current_value = 0
 @app.route('/')
 def inicio():
@@ -20,6 +20,7 @@ def increment():
     '''
     global current_value
     current_value += 1
+    '''devolviendo el valor actual de la variable "current_value" como una cadena de texto (str).'''
     return str(current_value)
 
 @app.route("/decrement")
@@ -40,6 +41,10 @@ def reset():
 
 @app.route('/info', methods=['GET', 'POST'])
 def editProfile():
+    '''
+    La función render_template() en Flask se utiliza para renderizar plantillas 
+    HTML utilizando el motor de plantillas Jinja2
+    '''
     return render_template('public/pages/info.html')
 
 
@@ -47,15 +52,23 @@ def editProfile():
 # Redireccionando cuando la página no existe
 @app.errorhandler(404)
 def not_found(error):
-    return render_template('404.html', error=error)
+    'url_for() se usa para generar las URLs basadas en los nombres de las funciones definidas'
+    return redirect(url_for('inicio'))
 
 
 @app.errorhandler(500)
 def server_error(error):
-    return render_template('500.html', error=error)
+    ''' 
+    La función redirect de Flask se usa para redirigir a una ruta en especifico (inicio).
+     '''
+    return redirect(url_for('inicio'))
 
 
 
 # Ejecutando el objeto Flask
 if __name__ == '__main__':
+    '''
+    app.run(debug=True, port=5000) se utiliza para iniciar el servidor Flask
+    y comenzar a escuchar las solicitudes entrantes
+    '''
     app.run(debug=True, port=5000)
